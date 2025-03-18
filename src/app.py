dc22207ebe768c5b0f7ec30d35c00e50b7e090a5
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2025-03-18 21:27:10 +0100

    refactor: structure update

--- src/app.py
+++ src/app.py
@@ -5,14 +5,11 @@ from routes import shorten_url, redirect_to_original_url
 
 app = Flask(__name__)
 
-# Load configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize extensions
 db.init_app(app)
 
-# Register routes
 app.add_url_rule("/shorten", "shorten_url", shorten_url, methods=["POST"])
 app.add_url_rule("/<short_code>", "redirect_to_original_url", redirect_to_original_url)
 
